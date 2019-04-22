898fa09c9c4f5641a74ff5fd34fdcb51e2638e95
Eduard Nikolenko <info@eduardnikolenko.com>
2019-04-22 16:56:27 +0300

    Fix parse

--- src/filters/dateParse.ts
+++ src/filters/dateParse.ts
@@ -6,34 +6,34 @@ const parse = (input: string, format: string, key: string): string => {
 
 export const dateParse = (input: string, format: string = 'YYYY-MM-DD HH.mm.ss', { epoch = 2000 } = {}): Date => {
   let year = 2000;
-  if (input.includes('YYYY')) {
+  if (format.includes('YYYY')) {
     year = parseInt(parse(input, format, 'YYYY'), 10);
   } else if (input.includes('YY')) {
     year = parseInt(parse(input, format, 'YY'), 10) + epoch;
   }
 
   let month = 0;
-  if (input.includes('MM')) {
+  if (format.includes('MM')) {
     month = parseInt(parse(input, format, 'MM'), 10) - 1;
   }
 
   let date = 1;
-  if (input.includes('DD')) {
+  if (format.includes('DD')) {
     date = parseInt(parse(input, format, 'DD'), 10);
   }
 
   let hours = 0;
-  if (input.includes('HH')) {
+  if (format.includes('HH')) {
     hours = parseInt(parse(input, format, 'HH'), 10);
   }
 
   let minutes = 0;
-  if (input.includes('mm')) {
+  if (format.includes('mm')) {
     minutes = parseInt(parse(input, format, 'mm'), 10);
   }
 
   let seconds = 0;
-  if (input.includes('ss')) {
+  if (format.includes('ss')) {
     seconds = parseInt(parse(input, format, 'ss'), 10);
   }
 
